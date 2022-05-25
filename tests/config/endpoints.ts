c48cc559c227c046ef1736638b8d265cdbc87d80
damevi <maciej.urlo@gmail.com>
2022-05-25 11:06:30 +0200

    refactor: make tests independent

--- tests/config/endpoints.ts
+++ tests/config/endpoints.ts
@@ -2,3 +2,4 @@ require("dotenv").config();
 
 export const OAUTH_ENDPOINT = "/oauth/token";
 export const WORKFLOW_ENDPOINT = `/projects/${process.env.PROJECT_ID}/workflows`;
+export const TASKS_ENDPOINT = "/tasks";
--- tests/worfkflows.spec.ts
+++ tests/worfkflows.spec.ts
@@ -1,32 +1,43 @@
-import { WORKFLOW_ENDPOINT } from "./config/endpoints";
+import { APIResponse } from "@playwright/test";
 import { test, expect } from "./fixtures/auth";
 
+import { TASKS_ENDPOINT, WORKFLOW_ENDPOINT } from "./config/endpoints";
+
+import { createNewWorkflow } from "./support/utilts";
+import { NEW_WORKFLOW_DATA, WORKFLOW_TASKS } from "./support/test_data";
+
+
 test.describe("Worfklows", () => {
-  let workflowId: string;
+  let currentWorkflow: APIResponse;
+  let currentWorkflowId: string;
 
-  test("Creates a workflow", async ({ context }) => {
-    const req = {
-      name: "QA coding challenge workflow",
-      description: "Workflow description",
-    };
+  test.beforeEach(async ({ context }) => {
+    currentWorkflow = await createNewWorkflow(context);
+    const { data } = await currentWorkflow.json();
+    currentWorkflowId = data.id;
+  });
 
-    const newWorkflow = await context.request.post(WORKFLOW_ENDPOINT, {
-      data: req,
+  test("Creates a workflow", async () => {
+    expect(currentWorkflow.ok()).toBeTruthy();
+    const { data } = await currentWorkflow.json();
+    expect(data).toMatchObject(NEW_WORKFLOW_DATA);
+  });
+
+  test("Adds workflow tasks", async ({ context }) => {
+    const tasksEndpoint = `${WORKFLOW_ENDPOINT}/${currentWorkflowId}${TASKS_ENDPOINT}`;
+
+    const res = await context.request.post(tasksEndpoint, {
+      data: WORKFLOW_TASKS,
     });
-    expect(newWorkflow.ok()).toBeTruthy();
 
-    const { data } = await newWorkflow.json();
-    workflowId = data.id;
-
-    expect(data).toMatchObject(req);
+    expect(res.ok()).toBeTruthy();
   });
 
   test.afterEach(async ({ context }) => {
-    console.log("ID", workflowId);
-    const resp = await context.request.delete(
-      `${WORKFLOW_ENDPOINT}/${workflowId}`
+    const res = await context.request.delete(
+      `${WORKFLOW_ENDPOINT}/${currentWorkflowId}`
     );
 
-    expect(resp.ok()).toBeTruthy();
+    expect(res.ok()).toBeTruthy();
   });
 });
